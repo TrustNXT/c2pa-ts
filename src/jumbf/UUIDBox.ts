c7243e02cd95505928d9c352b3cab35fbd832578
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-07-18 10:13:04 +0200

    UUIDBox: Define schema

--- src/jumbf/UUIDBox.ts
+++ src/jumbf/UUIDBox.ts
@@ -1,13 +1,46 @@
+import * as bin from 'typed-binary';
 import { BinaryHelper } from '../util';
 import { Box } from './Box';
+import { BoxSchema } from './BoxSchema';
+import * as schemata from './schemata';
+
+class UUIDBoxSchema extends BoxSchema<UUIDBox> {
+    readonly uuid = schemata.uuid;
+
+    readContent(input: bin.ISerialInput, type: string, length: number): UUIDBox {
+        if (type != UUIDBox.typeCode) throw new Error(`UUIDBox: Unexpected type ${type}`);
+
+        const uuid = this.uuid.read(input);
+        const content = [];
+        for (let i = 0; i != length - 4 - 4 - 16; i++) {
+            content.push(input.readByte());
+        }
+        const box = new UUIDBox();
+
+        box.uuid = uuid;
+        box.content = new Uint8Array(content);
+
+        return box;
+    }
+
+    writeContent(output: bin.ISerialOutput, value: UUIDBox): void {
+        this.uuid.write(output, value.uuid);
+        value.content?.forEach(byte => output.writeByte(byte));
+    }
+
+    measureContent(value: UUIDBox, measurer: bin.IMeasurer): bin.IMeasurer {
+        return measurer.add(this.uuid.measure(value.uuid).size + (value.content ? value.content.length : 0));
+    }
+}
 
 export class UUIDBox extends Box {
     public static readonly typeCode = 'uuid';
+    public static readonly schema = new UUIDBoxSchema();
     public uuid: Uint8Array = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     public content?: Uint8Array;
 
     constructor() {
-        super(UUIDBox.typeCode);
+        super(UUIDBox.typeCode, UUIDBox.schema);
     }
 
     public parse(buf: Uint8Array) {
---
+++ tests/jumbf/UUIDBox.test.ts
@@ -0,0 +1,127 @@
+import assert from 'assert';
+import * as bin from 'typed-binary';
+import { UUIDBox } from '../../src/jumbf/UUIDBox';
+import { BinaryHelper } from '../../src/util';
+
+describe('UUIDBox Tests', function () {
+    this.timeout(0);
+
+    describe('Minimal', function () {
+        const uuidString = '6332637300110010800000aa00389b71';
+        const serializedString = '00000018757569646332637300110010800000aa00389b71';
+
+        it('serialization', async function () {
+            const box = new UUIDBox();
+            box.uuid = BinaryHelper.fromHexString(uuidString);
+            box.content = new Uint8Array();
+
+            // fetch schema from the box
+            const schema = box.schema;
+
+            // write the box to a buffer
+            const length = schema.measure(box).size;
+            const buffer = Buffer.alloc(length);
+            const writer = new bin.BufferWriter(buffer, { endianness: 'big' });
+            schema.write(writer, box);
+
+            // verify that the expected buffer size was also used
+            assert.equal(buffer.length, writer.currentByteOffset, 'produced number of bytes differs');
+
+            // verify expected buffer contents
+            assert.equal(BinaryHelper.toHexString(buffer), serializedString);
+        });
+
+        // Special case for empty content
+        // This wouldn't be needed if `content` wasn't allowed to be
+        // `undefined` which should perhaps be changed (TODO).
+        it('serialization with undefined content', async function () {
+            const box = new UUIDBox();
+            box.uuid = BinaryHelper.fromHexString(uuidString);
+            box.content = undefined;
+
+            // fetch schema from the box
+            const schema = box.schema;
+
+            // write the box to a buffer
+            const length = schema.measure(box).size;
+            const buffer = Buffer.alloc(length);
+            const writer = new bin.BufferWriter(buffer, { endianness: 'big' });
+            schema.write(writer, box);
+
+            // verify that the expected buffer size was also used
+            assert.equal(buffer.length, writer.currentByteOffset, 'produced number of bytes differs');
+
+            // verify expected buffer contents
+            assert.equal(BinaryHelper.toHexString(buffer), serializedString);
+        });
+
+        it('deserialization', async function () {
+            const buffer = BinaryHelper.fromHexString(serializedString);
+
+            // fetch schema from the box class
+            const schema = UUIDBox.schema;
+
+            // read the box from the buffer
+            const reader = new bin.BufferReader(buffer, { endianness: 'big' });
+            const box = schema.read(reader);
+
+            // verify that the expected buffer size was also used
+            assert.equal(reader.currentByteOffset, buffer.length, 'consumed number of bytes differs');
+
+            // validate resulting box
+            if (!(box instanceof UUIDBox)) assert.fail('resulting box has wrong type');
+            assert.ok(box.uuid);
+            assert.equal(BinaryHelper.toHexString(box.uuid), uuidString);
+            assert.ok(box.content);
+            assert.equal(BinaryHelper.toHexString(box.content), '');
+        });
+    });
+
+    describe('With Content', function () {
+        const uuidString = '6332637300110010800000aa00389b71';
+        const contentString = '7465737420646174610a';
+        const serializedString = '00000022757569646332637300110010800000aa00389b717465737420646174610a';
+
+        it('serialization', async function () {
+            const box = new UUIDBox();
+            box.uuid = BinaryHelper.fromHexString(uuidString);
+            box.content = BinaryHelper.fromHexString(contentString);
+
+            // fetch schema from the box
+            const schema = box.schema;
+
+            // write the box to a buffer
+            const length = schema.measure(box).size;
+            const buffer = Buffer.alloc(length);
+            const writer = new bin.BufferWriter(buffer, { endianness: 'big' });
+            schema.write(writer, box);
+
+            // verify that the expected buffer size was also used
+            assert.equal(buffer.length, writer.currentByteOffset, 'produced number of bytes differs');
+
+            // verify expected buffer contents
+            assert.equal(BinaryHelper.toHexString(buffer), serializedString);
+        });
+
+        it('deserialization', async function () {
+            const buffer = BinaryHelper.fromHexString(serializedString);
+
+            // fetch schema from the box class
+            const schema = UUIDBox.schema;
+
+            // read the box from the buffer
+            const reader = new bin.BufferReader(buffer, { endianness: 'big' });
+            const box = schema.read(reader);
+
+            // verify that the expected buffer size was also used
+            assert.equal(reader.currentByteOffset, buffer.length, 'consumed number of bytes differs');
+
+            // validate resulting box
+            if (!(box instanceof UUIDBox)) assert.fail('resulting box has wrong type');
+            assert.ok(box.uuid);
+            assert.equal(BinaryHelper.toHexString(box.uuid), uuidString);
+            assert.ok(box.content);
+            assert.equal(BinaryHelper.toHexString(box.content), contentString);
+        });
+    });
+});
