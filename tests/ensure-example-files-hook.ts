e57189a82467de1c85e02c2472c282bf74103b79
Andreas Reich <andreas.reich@trustnxt.com>
2024-07-18 11:15:28 +0200

    Tests: Move public-testfiles into test fixtures directory

--- tests/ensure-example-files-hook.ts
+++ tests/ensure-example-files-hook.ts
@@ -1,15 +1,15 @@
-import { existsSync } from 'fs';
-import { normalize } from 'path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 function beforeAll() {
     // expected location with the test file repo
-    const testfilesDir = normalize(import.meta.filename + '/../../public-testfiles');
+    const testfilesDir = path.join(import.meta.dirname, 'fixtures/public-testfiles');
 
     // ensure that the Git repo with example-files is already present
-    if (!existsSync(testfilesDir)) {
+    if (!fs.existsSync(testfilesDir)) {
         process.stderr.write(`Git repo with example-files not present in ${testfilesDir}.\n`);
         process.stderr.write(
-            'Use `git clone git@github.com:c2pa-org/public-testfiles` in the project root to retrieve them.\n',
+            'Use `git clone --depth=1 git@github.com:c2pa-org/public-testfiles tests/fixtures/public-testfiles` in the project root to retrieve them.\n',
         );
         process.exit(1);
     }
