e9d0b202259808b630164abe601b708aabb51ba3
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-07-19 19:41:05 +0200

    EmbeddedFileDescriptionBox: Define schema

--- src/jumbf/EmbeddedFileDescriptionBox.ts
+++ src/jumbf/EmbeddedFileDescriptionBox.ts
@@ -1,13 +1,57 @@
+import * as bin from 'typed-binary';
 import { BinaryHelper } from '../util';
 import { Box } from './Box';
+import { BoxSchema } from './BoxSchema';
+
+class EmbeddedFileDescriptionBoxSchema extends BoxSchema<EmbeddedFileDescriptionBox> {
+    readonly flags = bin.byte;
+    readonly fileName = bin.string;
+    readonly mediaType = bin.string;
+
+    readContent(
+        input: bin.ISerialInput,
+        type: string,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        length: number,
+    ): EmbeddedFileDescriptionBox {
+        if (type != EmbeddedFileDescriptionBox.typeCode)
+            throw new Error(`EmbeddedFileDescriptionBox: Unexpected type ${type}`);
+
+        const flags = this.flags.read(input);
+
+        const box = new EmbeddedFileDescriptionBox();
+
+        box.mediaType = this.mediaType.read(input);
+        if (flags & 1) {
+            box.fileName = this.fileName.read(input);
+        }
+
+        return box;
+    }
+
+    writeContent(output: bin.ISerialOutput, value: EmbeddedFileDescriptionBox): void {
+        this.flags.write(output, value.fileName ? 1 : 0);
+        this.mediaType.write(output, value.mediaType ?? '');
+        if (value.fileName) this.fileName.write(output, value.fileName);
+    }
+
+    measureContent(value: EmbeddedFileDescriptionBox, measurer: bin.IMeasurer): bin.IMeasurer {
+        return measurer.add(
+            1 + // flags
+                this.mediaType.measure(value.mediaType ?? '').size +
+                (value.fileName ? this.fileName.measure(value.fileName).size : 0),
+        );
+    }
+}
 
 export class EmbeddedFileDescriptionBox extends Box {
     public static readonly typeCode = 'bfdb';
+    public static readonly schema = new EmbeddedFileDescriptionBoxSchema();
     public mediaType?: string;
     public fileName?: string;
 
     constructor() {
-        super(EmbeddedFileDescriptionBox.typeCode);
+        super(EmbeddedFileDescriptionBox.typeCode, EmbeddedFileDescriptionBox.schema);
     }
 
     public parse(buf: Uint8Array) {
---
+++ tests/jumbf/EmbeddedFileDescriptionBox.test.ts
@@ -0,0 +1,100 @@
+import assert from 'assert';
+import * as bin from 'typed-binary';
+import { EmbeddedFileDescriptionBox } from '../../src/jumbf';
+import { BinaryHelper } from '../../src/util';
+
+describe('EmbeddedFileDescriptionBox Tests', function () {
+    this.timeout(0);
+
+    describe('Without Filename', function () {
+        const mediaType = 'video/mp4';
+        const serializedString = '000000136266646200766964656f2f6d703400';
+
+        it('serialization', async function () {
+            const box = new EmbeddedFileDescriptionBox();
+            box.mediaType = mediaType;
+
+            // fetch schema from the box
+            const schema = box.schema;
+
+            // write the box to a buffer
+            const length = schema.measure(box).size;
+            const buffer = Buffer.alloc(length);
+            const writer = new bin.BufferWriter(buffer, { endianness: 'big' });
+            schema.write(writer, box);
+
+            // verify that the expected buffer size was also used
+            assert.equal(buffer.length, writer.currentByteOffset, 'produced number of bytes differs');
+
+            // verify expected buffer contents
+            assert.equal(BinaryHelper.toHexString(buffer), serializedString);
+        });
+
+        it('deserialization', async function () {
+            const buffer = BinaryHelper.fromHexString(serializedString);
+
+            // fetch schema from the box class
+            const schema = EmbeddedFileDescriptionBox.schema;
+
+            // read the box from the buffer
+            const reader = new bin.BufferReader(buffer, { endianness: 'big' });
+            const box = schema.read(reader);
+
+            // verify that the expected buffer size was also used
+            assert.equal(reader.currentByteOffset, buffer.length, 'consumed number of bytes differs');
+
+            // validate resulting box
+            if (!(box instanceof EmbeddedFileDescriptionBox)) assert.fail('resulting box has wrong type');
+            assert.ok(box.mediaType);
+            assert.equal(box.mediaType, mediaType);
+        });
+    });
+
+    describe('With Filename', function () {
+        const mediaType = 'video/mp4';
+        const fileName = 'holiday.mp4';
+        const serializedString = '0000001f6266646201766964656f2f6d703400686f6c696461792e6d703400';
+
+        it('serialization', async function () {
+            const box = new EmbeddedFileDescriptionBox();
+            box.mediaType = mediaType;
+            box.fileName = fileName;
+
+            // fetch schema from the box
+            const schema = box.schema;
+
+            // write the box to a buffer
+            const length = schema.measure(box).size;
+            const buffer = Buffer.alloc(length);
+            const writer = new bin.BufferWriter(buffer, { endianness: 'big' });
+            schema.write(writer, box);
+
+            // verify that the expected buffer size was also used
+            assert.equal(buffer.length, writer.currentByteOffset, 'produced number of bytes differs');
+
+            // verify expected buffer contents
+            assert.equal(BinaryHelper.toHexString(buffer), serializedString);
+        });
+
+        it('deserialization', async function () {
+            const buffer = BinaryHelper.fromHexString(serializedString);
+
+            // fetch schema from the box class
+            const schema = EmbeddedFileDescriptionBox.schema;
+
+            // read the box from the buffer
+            const reader = new bin.BufferReader(buffer, { endianness: 'big' });
+            const box = schema.read(reader);
+
+            // verify that the expected buffer size was also used
+            assert.equal(reader.currentByteOffset, buffer.length, 'consumed number of bytes differs');
+
+            // validate resulting box
+            if (!(box instanceof EmbeddedFileDescriptionBox)) assert.fail('resulting box has wrong type');
+            assert.ok(box.mediaType);
+            assert.equal(box.mediaType, mediaType);
+            assert.ok(box.fileName);
+            assert.equal(box.fileName, fileName);
+        });
+    });
+});
