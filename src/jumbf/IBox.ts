3f7f2bd3c2cb33bd1840e0b9f7d0deb7b63759d7
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-07-19 10:13:42 +0200

    IBox: Make the schema a property of the interface

--- src/jumbf/IBox.ts
+++ src/jumbf/IBox.ts
@@ -1,5 +1,8 @@
+import * as bin from 'typed-binary';
+
 export interface IBox {
     type: string;
+    schema: bin.ISchema<IBox>;
     parse(buf: Uint8Array, urlPrefix?: string): void;
     toString(prefix?: string): string;
 }
