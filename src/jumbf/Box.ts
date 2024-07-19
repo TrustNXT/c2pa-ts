00c2f44205ab48e3db0c5aab04f1bd5050391666
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-07-19 10:15:49 +0200

    Box: Implement mandatory schema property
    The schema is required to be given as parameter to the constructor,
    along with the type string. Typically, they are provided by one of
    the derived classes.

--- src/jumbf/Box.ts
+++ src/jumbf/Box.ts
@@ -1,10 +1,13 @@
+import * as bin from 'typed-binary';
 import { IBox } from './IBox';
 
 export class Box implements IBox {
     public readonly type: string;
+    public readonly schema: bin.ISchema<Box>;
 
-    constructor(type: string) {
+    constructor(type: string, schema: bin.ISchema<Box>) {
         this.type = type;
+        this.schema = schema;
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
