73f02c2df1def723a6c7a26e75f0a04d67c8c1b2
Andreas Reich <andreas.reich@trustnxt.com>
2024-07-23 18:32:21 +0200

    Split up BMFF hash assertion validation to reduce cyclomatic complexity

--- src/manifest/assertions/BMFFHashAssertion.ts
+++ src/manifest/assertions/BMFFHashAssertion.ts
@@ -1,4 +1,4 @@
-import { Asset, BMFF } from '../../asset';
+import { Asset, BMFF, BMFFBox } from '../../asset';
 import { HashAlgorithm } from '../../crypto';
 import * as JUMBF from '../../jumbf';
 import { BinaryHelper } from '../../util';
@@ -88,50 +88,9 @@ export class BMFFHashAssertion extends Assertion {
         const markers = new Set(asset.getTopLevelBoxes().map(box => box.offset));
 
         const exclusionRanges: HashExclusionRange[] = [];
-        exclusionLoop: for (const exclusion of this.exclusions) {
-            // A box matches an exclusion entry in the exclusions array if and only if all of the following conditions are met:
-
-            // The box’s location in the file exactly matches the exclusions-map entry’s xpath field.
-            const box = asset.getBoxByPath(exclusion.xpath);
+        for (const exclusion of this.exclusions) {
+            const box = await BMFFHashAssertion.getMatchingBoxForExclusion(exclusion, asset);
             if (!box) continue;
-
-            // If length is specified in the exclusions-map entry, the box’s length exactly matches the exclusions-map entry’s length field.
-            if (exclusion.length && box.size !== exclusion.length) continue;
-
-            // If version is specified in the exclusions-map entry, the box is a FullBox and the box’s version exactly matches the exclusions-map entry’s version field.
-            if (exclusion.version && !('version' in box.payload && box.payload.version === exclusion.version)) continue;
-
-            // If flags (byte array of exactly 3 bytes) is specified in the exclusions-map entry and the box is a FullBox.
-            if (exclusion.flags) {
-                if (!('flags' in box.payload)) continue;
-
-                const boxFlags = box.payload.flags as Uint8Array;
-                if (exclusion.flags.length !== 3 || boxFlags.length !== 3) continue;
-
-                // If exact is set to true or not specified, the box’s flags (bit(24), i.e., 3 bytes) also exactly matches the exclusions-map entry’s flags field.
-                if (exclusion.exact === undefined || exclusion.exact) {
-                    if (!BinaryHelper.bufEqual(exclusion.flags, boxFlags)) continue;
-                } else {
-                    // If exact is set to false, the bitwise-and of the box’s flags (bit(24), i.e., 3 bytes) with the exclusions-map entry’s flags field exactly matches the exclusions-map entry’s flags field.
-                    for (let i = 0; i < 3; i++) {
-                        if ((exclusion.flags[i] & boxFlags[i]) !== exclusion.flags[i]) {
-                            continue exclusionLoop;
-                        }
-                    }
-                }
-            }
-
-            // If data (array of objects) is specified in the exclusions-map entry, then for each item in the array, the box’s binary data at that item’s relative byte offset field exactly matches that item’s bytes field.
-            for (const data of exclusion.data ?? []) {
-                if (
-                    !BinaryHelper.bufEqual(
-                        data.value,
-                        await asset.getDataRange(box.offset + data.offset, data.value.length),
-                    )
-                ) {
-                    continue exclusionLoop;
-                }
-            }
 
             if (exclusion.subset) {
                 for (const subset of exclusion.subset) {
@@ -169,5 +128,57 @@ export class BMFFHashAssertion extends Assertion {
         } else {
             return ValidationResult.error(ValidationStatusCode.AssertionBMFFHashMismatch, this.sourceBox);
         }
+    }
+
+    private static async getMatchingBoxForExclusion(
+        exclusion: Exclusion,
+        asset: BMFF,
+    ): Promise<BMFFBox<object> | undefined> {
+        // A box matches an exclusion entry in the exclusions array if and only if all of the following conditions are met:
+
+        // The box’s location in the file exactly matches the exclusions-map entry’s xpath field.
+        const box = asset.getBoxByPath(exclusion.xpath);
+        if (!box) return undefined;
+
+        // If length is specified in the exclusions-map entry, the box’s length exactly matches the exclusions-map entry’s length field.
+        if (exclusion.length && box.size !== exclusion.length) return undefined;
+
+        // If version is specified in the exclusions-map entry, the box is a FullBox and the box’s version exactly matches the exclusions-map entry’s version field.
+        if (exclusion.version && !('version' in box.payload && box.payload.version === exclusion.version))
+            return undefined;
+
+        // If flags (byte array of exactly 3 bytes) is specified in the exclusions-map entry and the box is a FullBox.
+        if (exclusion.flags) {
+            if (!('flags' in box.payload)) return undefined;
+
+            const boxFlags = box.payload.flags as Uint8Array;
+            if (exclusion.flags.length !== 3 || boxFlags.length !== 3) return undefined;
+
+            // If exact is set to true or not specified, the box’s flags (bit(24), i.e., 3 bytes) also exactly matches the exclusions-map entry’s flags field.
+            if (exclusion.exact === undefined || exclusion.exact) {
+                if (!BinaryHelper.bufEqual(exclusion.flags, boxFlags)) return undefined;
+            } else {
+                // If exact is set to false, the bitwise-and of the box’s flags (bit(24), i.e., 3 bytes) with the exclusions-map entry’s flags field exactly matches the exclusions-map entry’s flags field.
+                for (let i = 0; i < 3; i++) {
+                    if ((exclusion.flags[i] & boxFlags[i]) !== exclusion.flags[i]) {
+                        return undefined;
+                    }
+                }
+            }
+        }
+
+        // If data (array of objects) is specified in the exclusions-map entry, then for each item in the array, the box’s binary data at that item’s relative byte offset field exactly matches that item’s bytes field.
+        for (const data of exclusion.data ?? []) {
+            if (
+                !BinaryHelper.bufEqual(
+                    data.value,
+                    await asset.getDataRange(box.offset + data.offset, data.value.length),
+                )
+            ) {
+                return undefined;
+            }
+        }
+
+        return box;
     }
 }
