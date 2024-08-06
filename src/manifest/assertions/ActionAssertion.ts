e2f6b335b4c7af5586e47d11938347792908d74f
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2024-08-06 10:19:08 +0200

    ActionAssertion: Implement mapping to a JUMBF box

--- src/manifest/assertions/ActionAssertion.ts
+++ src/manifest/assertions/ActionAssertion.ts
@@ -127,4 +127,75 @@ export class ActionAssertion extends Assertion {
             }
         }
     }
+
+    public generateJUMBFBoxForContent(claim: Claim): JUMBF.IBox {
+        const box = new JUMBF.CBORBox();
+        switch (this.label) {
+            case AssertionLabels.actions:
+                box.content = this.mapActionsToCBORData(claim);
+                break;
+            case AssertionLabels.actionsV2:
+                box.content = this.mapActionsV2ToCBORData(claim);
+                break;
+            default:
+                throw new Error('Invalid assertion label');
+        }
+        return box;
+    }
+
+    private mapActionsToCBORData(claim: Claim): RawActionsMap {
+        return {
+            actions: this.actions.map(action => {
+                const res: RawAction = { action: action.action };
+                if (action.reason) res.reason = action.reason;
+                if (action.parameters) {
+                    res.parameters = {};
+                    for (const [name, value] of Object.entries(action.parameters)) {
+                        if (value === undefined) continue;
+                        if (name === 'ingredients') continue;
+                        res.parameters[name] = value;
+                    }
+
+                    if (action.parameters.ingredients && action.parameters.ingredients.length !== 0) {
+                        if (action.parameters.ingredients.length !== 1)
+                            throw new Error('Multiple ingredients not supported');
+                        const hashedURI = action.parameters.ingredients[0];
+                        res.parameters.ingredient = {
+                            hash: hashedURI.hash,
+                            url: hashedURI.uri,
+                        };
+                        if (hashedURI.algorithm !== claim.defaultAlgorithm)
+                            res.parameters.ingredient.alg = Claim.reverseMapHashAlgorithm(hashedURI.algorithm);
+                    }
+                }
+                return res;
+            }),
+            // TODO: metadata
+        };
+    }
+
+    private mapActionsV2ToCBORData(claim: Claim): RawActionsMapV2 {
+        return {
+            actions: this.actions.map(action => {
+                const res: RawActionV2 = { action: action.action };
+                if (action.reason) res.reason = action.reason;
+                if (action.parameters) {
+                    res.parameters = {};
+                    for (const [name, value] of Object.entries(action.parameters)) {
+                        if (name === 'ingredients') continue;
+                        res.parameters[name] = value;
+                    }
+
+                    if (action.parameters.ingredients) {
+                        res.parameters.ingredients = action.parameters.ingredients.map(hashedURI =>
+                            claim.reverseMapHashedURI(hashedURI),
+                        );
+                    }
+                }
+                return res;
+            }),
+            // TODO: templates
+            // TODO: metadata
+        };
+    }
 }
