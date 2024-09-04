ebc2cc4f805d55a69d9ceabe7dec8a013d194fe0
Andreas Reich <andreas.reich@trustnxt.com>
2024-09-04 16:37:44 +0200

    Timestamping: Remove redundant OID code

--- src/rfc3161/Timestamp.ts
+++ src/rfc3161/Timestamp.ts
@@ -6,17 +6,6 @@ import { TimestampProvider } from './TimestampProvider';
 export class Timestamp {
     private constructor() {}
 
-    private static getOID(algorithm: HashAlgorithm) {
-        switch (algorithm) {
-            case 'SHA-256':
-                return '2.16.840.1.101.3.4.2.1';
-            case 'SHA-384':
-                return '2.16.840.1.101.3.4.2.2';
-            case 'SHA-512':
-                return '2.16.840.1.101.3.4.2.3';
-        }
-    }
-
     public static async getTimestamp(
         provider: TimestampProvider,
         payload: Uint8Array,
@@ -26,7 +15,7 @@ export class Timestamp {
             version: 1,
             messageImprint: new MessageImprint({
                 hashAlgorithm: new AlgorithmIdentifier({
-                    algorithmId: this.getOID(algorithm),
+                    algorithmId: Crypto.getHashAlgorithmOID(algorithm),
                     algorithmParams: new asn1js.Null(),
                 }),
                 hashedMessage: new asn1js.OctetString({ valueHex: await Crypto.digest(payload, algorithm) }),
