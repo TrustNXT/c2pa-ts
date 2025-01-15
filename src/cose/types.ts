f26a3a0c24036a734d789dcb0adbeff6774fc6e8
Andreas Reich <andreas.reich@trustnxt.com>
2025-01-15 10:31:20 +0100

    Implement v2 timestamps (sigTst2)

--- src/cose/types.ts
+++ src/cose/types.ts
@@ -1,13 +1,18 @@
+import * as pkijs from 'pkijs';
+
+export interface TstContainer {
+    tstTokens: {
+        val: Uint8Array;
+    }[];
+}
+
 export interface HeaderBucket {
     [key: string]: unknown;
     '1'?: number; // alg
     '33'?: Uint8Array[] | Uint8Array; // x5chain
     x5chain?: Uint8Array[] | Uint8Array; // legacy named x5chain
-    sigTst?: {
-        tstTokens: {
-            val: Uint8Array;
-        }[];
-    };
+    sigTst?: TstContainer;
+    sigTst2?: TstContainer;
     rVals?: {
         ocspVals?: Uint8Array[];
     };
@@ -29,4 +34,14 @@ export type CoseSignature = [
 export enum AdditionalEKU {
     any = '2.5.29.37.0',
     documentSigning = '1.3.6.1.5.5.7.3.36',
+}
+
+export enum TimestampVersion {
+    V1 = 1,
+    V2 = 2,
+}
+
+export interface TimestampToken {
+    response: pkijs.TimeStampResp;
+    version: TimestampVersion;
 }
