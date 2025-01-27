5784b2083b3c8b07ca329bb2d21f0d04e8797a09
Karlo Benčić <karlo.bencic@gmail.com>
2025-01-27 15:41:06 +0100

    fix version

--- src/util/Version.ts
+++ src/util/Version.ts
@@ -1,6 +1,6 @@
 export class Version {
     public static readonly productName = 'c2pa-ts';
-    public static readonly productVersion = '0.6.0-605f172';
+    public static readonly productVersion = '@VERSION@';
 
     private constructor() {}
 }
