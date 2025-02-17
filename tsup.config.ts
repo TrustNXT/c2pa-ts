b42fed0ffdf3ded223b95fb96340411f02cef19a
Karlo Benčić <karlo.bencic@gmail.com>
2025-02-17 10:42:40 +0100

    Simplify build process and remove prebuild/postbuild scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,8 @@
+import { execSync } from 'node:child_process';
 import { defineConfig } from 'tsup';
+
+const gitRevision = execSync('git rev-parse --short HEAD').toString().trim();
+const version = process.env.npm_package_version;
 
 export default defineConfig({
     entry: {
@@ -9,6 +13,9 @@ export default defineConfig({
         'jumbf/index': 'src/jumbf/index.ts',
         'manifest/index': 'src/manifest/index.ts',
         'rfc3161/index': 'src/rfc3161/index.ts',
+    },
+    define: {
+        'process.env.VERSION': `"${version}-${gitRevision}"`,
     },
     format: ['esm'],
     dts: true,
